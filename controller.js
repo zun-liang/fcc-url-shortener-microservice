f389e91b6a0c991e86e8da58ff3d8b56a660517e
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-09-10 11:17:54 -0500

    add shortenUrl and redirectToOriginalUrl functions

--- controller.js
+++ controller.js
@@ -1,1 +1,41 @@
+import { StatusCodes } from "http-status-codes";
+import Url from "./model.js";
+import shortid from "shortid";
 
+export const shortenUrl = async (req, res) => {
+  try {
+    const originalUrl = req.body.url;
+    const existingUrl = await Url.findOne({ original_url: originalUrl });
+    if (existingUrl) {
+      return res
+        .status(StatusCodes.OK)
+        .json({ original_url: originalUrl, short_url: existingUrl.short_url });
+    }
+
+    const shortUrl = shortid.generate();
+    const newUrl = await Url.create({
+      original_url: originalUrl,
+      short_url: shortUrl,
+    });
+    res
+      .status(StatusCodes.OK)
+      .json({ original_url: newUrl.original_url, short_url: newUrl.short_url });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.BAD_REQUEST).json({ error });
+  }
+};
+
+export const redirectToOriginalUrl = async (req, res) => {
+  try {
+    const { shortUrl } = req.params;
+    const targetUrl = await Url.findOne({ short_url: shortUrl });
+    if (targetUrl) {
+      return res.redirect(targetUrl.original_url);
+    }
+    res.status(StatusCodes.BAD_REQUEST).json({ error: "invalid url" });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.BAD_REQUEST).json({ error });
+  }
+};
